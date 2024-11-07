78f7f08ee2e596b2bdbda4dc1c0eff1ef1a483bb
Левшин Павел Игоревич <plevshin@alfababk.tu>
2024-11-07 13:16:55 +0100

    init jest

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,30 +1,7 @@
-import * as chaiModule from 'chai';
-import chaiHttp from 'chai-http';
+import { describe, test, expect } from '@jest/globals';
 
-const chai = chaiModule.use(chaiHttp);
-
-import app from '../src';
-
-const auth = [
-    process.env.BASIC_AUTH_LOGIN as string,
-    process.env.BASIC_AUTH_PASSWORD as string,
-] as const;
-
-chai.should();
-
-describe('Objects methods', function () {
-    it('it should GET all the Objects', (done) => {
-        const server = chai.request.execute(app).keepOpen();
-        server
-            .get('/object')
-            .auth(...auth)
-            .end((_, res) => {
-                //console.log(res);
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                done();
-            });
-
-        server.close();
+describe('Sum function', () => {
+    test('Returns correct value', () => {
+        expect(5).toEqual(5);
     });
 });
